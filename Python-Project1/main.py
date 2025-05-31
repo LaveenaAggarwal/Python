977d6a22931b10ad5b7d388eff748ebef941bd8b
LaveenaAggarwal <er.laveenaaggarwal08@gmail.com>
2025-05-31 12:22:16 +0530

    Update main.py

--- Python-Project1/main.py
+++ Python-Project1/main.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Wecome, Laveena!!"
+    return "Wecome, Laveena Aggarwal!!"
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=80)
